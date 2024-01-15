5164b2359b4372acfaf41d5a7416496cced7aca4
Bingbing Wei <88399275+b1ngggg@users.noreply.github.com>
2024-01-15 09:34:28 +0000

    1

--- AL Game/Assets/Scripts/NPCcontrol.cs
+++ AL Game/Assets/Scripts/NPCcontrol.cs
@@ -4,46 +4,52 @@ using UnityEngine;
 
 public class NPCcontrol : MonoBehaviour
 {
-    private bool Ismove=true;
-    public GameObject Player;
-    private bool Isattack = false;
-    public Animator Ani;
-    void Start()
-    {
-        InvokeRepeating("Aset", 5, 5);
-        Ani.SetBool("run", Ismove);
-        //Animator for future use
-    }
-    public void Aset()
-    {
-        Ismove = !Ismove;
-        Ani.SetBool("run", Ismove);
-    }
-    void Update()
-    {
-        if (!Isattack)
-        {
-            if (Ismove)
-            {
-                transform.Translate(Vector3.forward * 3 * Time.deltaTime); //change the number to modify the speed of enemy moving forward
-            }
-            else
-            {
-                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
-                {
-                    Isattack = true;
-                    Ani.SetBool("run", true);
-                    CancelInvoke("Aset");
-                    gameObject.transform.LookAt(Player.transform.localPosition);
-                    
-                }
-            }
-        }
-        else
-        {
-            gameObject.transform.LookAt(Player.transform.localPosition);
-            transform.Translate(Vector3.forward * 10 * Time.deltaTime); //change the number to modify the speed of enemy chasing
-        }
-        
-    }
+	private bool Ismove = true;
+	public GameObject Player;
+	private bool Isattack = false;
+	public Animator animator;
+	private float moveSpeed = 3f; // Speed when moving normally
+	private float chaseSpeed = 10f; // Speed when chasing the player
+
+
+	void Start()
+	{
+		InvokeRepeating("ToggleMovement", 5, 5);
+		// Initial setting of the speed in the Animator
+		animator.SetFloat("Speed", moveSpeed);
+	}
+
+	public void ToggleMovement()
+	{
+		Ismove = !Ismove;
+	}
+
+	void Update()
+	{
+		if (!Isattack)
+		{
+			if (Ismove)
+			{
+				transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+				animator.SetFloat("Speed", moveSpeed);
+			}
+			else
+			{
+				animator.SetFloat("Speed", 0f); // Set speed to 0 when not moving
+			}
+
+			if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+			{
+				Isattack = true;
+				gameObject.transform.LookAt(Player.transform.localPosition);
+				CancelInvoke("ToggleMovement");
+			}
+		}
+		else
+		{
+			gameObject.transform.LookAt(Player.transform.localPosition);
+			transform.Translate(Vector3.forward * chaseSpeed * Time.deltaTime);
+			animator.SetFloat("Speed", chaseSpeed);
+		}
+	}
 }
